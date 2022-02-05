c3c4a0a6245f0727fc9044a9f58e84ea8392ef30
Sachnun Kusmayadi <sachnun12@gmail.com>
2022-02-05 15:55:07 +0700

    update

--- Pondok Laundry/My Project/Application.Designer.vb
+++ Pondok Laundry/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Pondok_Laundry.Login
+            Me.MainForm = Global.Pondok_Laundry.Splash
         End Sub
     End Class
 End Namespace
